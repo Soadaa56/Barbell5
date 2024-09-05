1e91abdf33c4044f2280f4ec50dd288c1fc47e04
soadaa <Soadhlh@gmail.com>
2024-09-05 16:54:14 -0400

    Native wind and supplied assets for project

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Barbell5</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-black">Barbell5</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue' }}>Profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
